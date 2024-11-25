5656b4b5e72b0716f4e012b6e54129fb910dedc0
Facundo Uriona <urionafacu@gmail.com>
2024-11-25 00:40:33 -0300

    feat(admin): register models

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from expenses.models import Expense, Group, GroupMember
+
+admin.site.register(Expense)
+admin.site.register(Group)
+admin.site.register(GroupMember)
